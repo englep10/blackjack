5f7e680b1a97c47f4f795d07b0468bbae8d91b0a
Philip Engles <philipengles@Philips-MacBook-Pro-2.local>
2015-07-13 11:37:48 -0400

    I have added the basic HTML UX I should need for playing a game. I have also added some barebones JS functionality, loaded my deck into an array as well as created empty arrays for the user hands and house hands. Working on the functions for the buttons at the moment.;

---
+++ blackjack.js
@@ -0,0 +1,31 @@
+var deck = ['Ac', 'Ad', 'Ah', 'As', '2c', '2d', '2h', '2s', '3c', '3d', '3h', '3s', '4c', '4d', '4h', '4s', '5c', '5d', '5h', '5s', '6c', '6d', '6h', '6s', '7c', '7d', '7h', '7s', '8c', '8d', '8h', '8s', '9c', '9d', '9h', '9s', 'Tc', 'Td', 'Th', 'Ts', 'Jc', 'Jd', 'Jh', 'Js', 'Qc', 'Qd', 'Qh', 'Qs', 'Kc', 'Kd', 'Kh', 'Ks'];
+console.log(deck);
+var userHand = [];
+var houseHand = [];
+
+// hitMe function | The hitMe function will take the top card from the shuffled deck and place it into the userHand array
+var hitMe = function(addCard){
+ var topCard =
+ var addCard = $("#userHand");
+
+}
+
+//
+
+
+// The Event listeners for my buttons using Jquery
+$("#hitMe").on("click", function() {
+
+});
+
+$("#stay").on("click", function() {
+
+});
+
+$("#resetGame").on("click", function() {
+
+});
+
+$("#startGame").on("click", function() {
+
+});

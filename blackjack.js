0600eb3d301036fe7c9be31a36adf03974ba69b5
Philip Engles <philipengles@Philips-MacBook-Pro-2.local>
2015-07-15 09:24:14 -0400

    updated the divs for

--- blackjack.js
+++ blackjack.js
@@ -1,6 +1,6 @@
 //Loaded 52 cards into the deck so that user/css can know what suit and value the cards are
-var deck = ['Ac', 'Ad', 'Ah', 'As', '2c', '2d', '2h', '2s', '3c', '3d', '3h', '3s', '4c', '4d', '4h', '4s', '5c', '5d', '5h', '5s', '6c', '6d', '6h', '6s', '7c', '7d', '7h', '7s', '8c', '8d', '8h', '8s', '9c', '9d', '9h', '9s', 'Tc', 'Td', 'Th', 'Ts', 'Jc', 'Jd', 'Jh', 'Js', 'Qc', 'Qd', 'Qh', 'Qs', 'Kc', 'Kd', 'Kh', 'Ks'];
-console.log(deck);
+// var deck = ['Ac', 'Ad', 'Ah', 'As', '2c', '2d', '2h', '2s', '3c', '3d', '3h', '3s', '4c', '4d', '4h', '4s', '5c', '5d', '5h', '5s', '6c', '6d', '6h', '6s', '7c', '7d', '7h', '7s', '8c', '8d', '8h', '8s', '9c', '9d', '9h', '9s', 'Tc', 'Td', 'Th', 'Ts', 'Jc', 'Jd', 'Jh', 'Js', 'Qc', 'Qd', 'Qh', 'Qs', 'Kc', 'Kd', 'Kh', 'Ks'];
+// console.log(deck);
 //Loaded 52 values of the deck into an array so that I can more easily fiddle around with the logic behind blackjack
 var pointValueDeck = [11, 11, 11, 11, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, ];
 console.log(pointValueDeck);
@@ -27,34 +27,102 @@ var startGame = function(init){
  userHand.push(card_3);
  var card_4 = randomDeck.shift();
  houseHand.push(card_4);
+ console.log(houseHand.length)
+ for(i=0; i < houseHand.length; i++){
+          $(".house_card").append("<div class='house_card" + i + "'>" + houseHand[i] + "</div>");
+        }
+ for(i=0; i < userHand.length; i++){
+          $(".user_card").append("<div class='user_card" + i + "'>" + userHand[i] + "</div>");
+        }
 
+
+ var userValue = eval(userHand.join('+'));
+ var houseValue = eval(houseHand.join('+'));
  console.log(userHand);
+ console.log(userValue);
  console.log(houseHand);
+ console.log(houseValue);
+ if (userValue === 21)
+    {
+      alert("Blackjack!")
+    }
+
 }
 
 // hitMe function | The hitMe function will take the top card from the shuffled deck and place it into the userHand array
 var hitMe = function(addCard){
  var addCard = randomDeck.shift();
  userHand.push(addCard);
- console.log(userHand);
+ var userValue = eval(userHand.join('+'));
+ var houseValue = eval(houseHand.join('+'));
+ if (userValue > 21){
+   alert("You busted you lose :(")
+ }
+ if (userValue === 21)
+    {
+      alert("I wouldnt hit if I were you")
+    }
+if ( houseValue < 16 && userValue < 21 )
+   {
+     var addCard = randomDeck.shift();
+     houseHand.push(addCard);
+     var houseValue = eval(houseHand.join('+'));
+     if ( houseValue > 21 )
+     {
+       alert("The dealer has busted! You win!")
+     }
+   }
 
+ console.log(userHand);
+ console.log(userValue);
+ console.log(houseHand)
+ console.log(houseValue)
 }
 
 //stay function | The stay function will be an alert to the game that the user has stopped the game and should check the values of the userHand against the houseHand to se which hand has won
 var stay = function(stopGame){
+  var userValue = eval(userHand.join('+'));
+  var houseValue = eval(houseHand.join('+'));
 
+  if ( houseValue < 16 && userValue > houseValue ) {
+    var addCard = randomDeck.shift();
+    houseHand.push(addCard);
+    var houseValue = eval(houseHand.join('+'));
+    console.log(houseValue);
+    if ( houseValue < userValue ) {
+      var addCard = randomDeck.shift();
+      houseHand.push(addCard);
+      var houseValue = eval(houseHand.join('+'));
+      console.log(houseValue);
+    }
+    if ( houseValue < userValue ) {
+      var addCard = randomDeck.shift();
+      houseHand.push(addCard);
+      var houseValue = eval(houseHand.join('+'));
+      console.log(houseValue);
+    }
+    if (houseValue > userValue && houseValue <= 21) {
+      alert("Sorry the Dealer won");
+    }
+  }
+  if ( userValue > houseValue && userValue < 21 ) {
+    alert("You Won!!!");
+  } else if (userValue == houseValue) {
+    alert("Sorry the House always wins");
+  }
+  if (houseValue > userValue && houseValue < 22) {
+    alert("Sorry the dealer won")
+  }
+    else if (houseValue > 21) {
+      alert("The Dealer Busted")
+    }
 
 }
 
 //resetGame function | The resetGame function will set both the userHand and houseHand to an empty Array
 var resetGame = function(clear){
-  userHand = [];
-  houseHand = [];
-  console.log(userHand);
-  console.log(houseHand);
+  window.location.reload(false);
 }
-
-
 
 
 
